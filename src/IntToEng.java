cfbbdb6a991f751df58193849ade1cf496116559
Minami Okuno <m.3.pguitar@gmail.com>
2018-05-17 15:36:06 +0900

    0~99まで英語に変換

--- src/IntToEng.java
+++ src/IntToEng.java
@@ -16,14 +16,15 @@ public class IntToEng {
     // 数値を英訳する変換するメソッド
     static String translateEng(int n) {
     	//新しい文字列
-    	String s="";
-    	if(n<=10) {
-    		s=onePlace(n);
-    	}else if(n%10<10) {
-    		
+    	String s1="";
+    	String s10="";
+    	//if(n<=10) {//10以下の時
+    	s1=onePlace(n%10);
+    	if(10 < n && n < 100) {//10より大きいかつ100未満の時
+    		s10=tenPlace(n/10);
+    		if(s10=="ten") {s10=teen(n%10); s1="";}
     	}
-    	
-        return s;
+        return s10+s1;
     }
     
     static String onePlace(int n) {
@@ -50,6 +51,55 @@ public class IntToEng {
     		return "nine";
     	case 10:
     		return "ten";
+    	}
+    	return "";
+    }
+    
+    static String teen(int n) {
+    	switch(n) {
+    	case 0:
+    		return "ten";
+    	case 1:
+    		return "eleven";
+    	case 2:
+    		return "twelve";
+    	case 3:
+    		return "thirteen";
+    	case 4:
+    		return "fourteen";
+    	case 5:
+    		return "fifteen";
+    	case 6:
+    		return "sixteen";
+    	case 7:
+    		return "seventeen";
+    	case 8:
+    		return "eighteen";
+    	case 9:
+    		return "nineteen";
+    	}
+    	return "";
+    }
+    static String tenPlace(int n) {
+    	switch(n) {
+    	case 1:
+    		return "ten";
+    	case 2:
+    		return "twenty";
+    	case 3:
+    		return "thirty";
+    	case 4:
+    		return "forty";
+    	case 5:
+    		return "fifty";
+    	case 6:
+    		return "sixty";
+    	case 7:
+    		return "seventy";
+    	case 8:
+    		return "eighty";
+    	case 9:
+    		return "ninety";
     	}
     	return "";
     }
--- src/IntToEngTest.java
+++ src/IntToEngTest.java
@@ -8,6 +8,9 @@ public class IntToEngTest {
 
 	int one = 1;
 	int ten = 10;
+	int fifteen = 15;
+	int twentyone = 21;
+	
 
 	@Test
 	public void onePlaceの1の表示(){
@@ -21,6 +24,20 @@ public class IntToEngTest {
 		IntToEng ite = new IntToEng();
 		String expected = "ten";
 		String actual =ite.onePlace(ten);
+		assertThat(actual,is(expected));
+	}
+	@Test
+	public void translateEngの15の表示(){
+		IntToEng ite = new IntToEng();
+		String expected = "fifteen";
+		String actual =ite.translateEng(fifteen);
+		assertThat(actual,is(expected));
+	}
+	@Test
+	public void translateEngの21の表示(){
+		IntToEng ite = new IntToEng();
+		String expected = "twentyone";
+		String actual =ite.translateEng(twentyone);
 		assertThat(actual,is(expected));
 	}
 }
