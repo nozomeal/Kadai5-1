10666d736580f517adf8e14084e0324c909711a5
sakamotonozomi <g15938sn@gm.tsuda.ac.jp>
2018-05-17 16:10:04 +0900

    100~999まで表示

--- src/IntToEngTest.java
+++ src/IntToEngTest.java
@@ -10,6 +10,9 @@ public class IntToEngTest {
 	int ten = 10;
 	int fifteen = 15;
 	int twentyone = 21;
+	int onetwothree = 123;
+	int onezerothree = 103;
+	int onezerozero = 100;
 	
 
 	@Test
@@ -38,6 +41,28 @@ public class IntToEngTest {
 		IntToEng ite = new IntToEng();
 		String expected = "twentyone";
 		String actual =ite.translateEng(twentyone);
+		assertThat(actual,is(expected));
+	}
+	
+	@Test
+	public void translateEngの123の表示(){
+		IntToEng ite = new IntToEng();
+		String expected = "one hundred twentythree";
+		String actual =ite.translateEng(onetwothree);
+		assertThat(actual,is(expected));
+	}
+	@Test
+	public void translateEngの103の表示(){
+		IntToEng ite = new IntToEng();
+		String expected = "one hundred three";
+		String actual =ite.translateEng(onezerothree);
+		assertThat(actual,is(expected));
+	}
+	@Test
+	public void translateEngの100の表示(){
+		IntToEng ite = new IntToEng();
+		String expected = "one hundred ";
+		String actual =ite.translateEng(onezerozero);
 		assertThat(actual,is(expected));
 	}
 }
